737ba547e2d256deecf0ebee2c69caec2dcfb9d4
leandro6969 <142300769+leandro6969@users.noreply.github.com>
2023-11-21 09:58:11 +0100

    aufgabe 1

--- script.js
+++ script.js
@@ -4,7 +4,11 @@ export function aufgabe01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    result.push(currentElement)
+    if (currentElement != "e") {
+    if (currentElement != "E")
+      result.push(currentElement)
+    }
   }
   return result.join("")
-}
+     }
+  
