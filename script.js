dd7ed146e459648c7646fc732c8a308dce76e014
leandro6969 <142300769+leandro6969@users.noreply.github.com>
2023-11-23 12:14:53 +0100

    23.11.2023

--- script.js
+++ script.js
@@ -13,7 +13,35 @@ export function aufgabe01(args) {
      }
   
 export function aufgabe02(args) {
+  return args.toUpperCase()
+} 
+
+export function aufgabe03(args){
+
   const input = args
-  return result 
-  const result = input.toUpperCase()
+  let result = 0 
+  for (let i = 0; i < input.length; i++) {
+  const currentElement = input[i]
+  if (currentElement === "e" || currentElement === "E") {
+  
+      result++
+  }
+}
+return result
+}
+
+
+
+export function aufgabe04(args){
+
+  const input = args
+  let result = 0 
+  for (let i = 0; i < input.length; i++) {
+  const currentElement = input[i]
+    if (currentElement === "e" || currentElement === "E") {
+  
+      result++
+  }
+}
+return result
 }
