c97546ca1e9b026bc8c85d9ea156afc8875c61f1
leandro6969 <142300769+leandro6969@users.noreply.github.com>
2023-12-07 12:14:00 +0100

    aufgabe 5 und 6 teilweise, es funktioren noch 2 Punkte nicht

--- script.js
+++ script.js
@@ -5,43 +5,63 @@ export function aufgabe01(args) {
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
     if (currentElement != "e") {
-    if (currentElement != "E")
-      result.push(currentElement)
+      if (currentElement != "E") result.push(currentElement)
     }
   }
   return result.join("")
-     }
-  
+}
+
 export function aufgabe02(args) {
   return args.toUpperCase()
-} 
-
-export function aufgabe03(args){
-
-  const input = args
-  let result = 0 
-  for (let i = 0; i < input.length; i++) {
-  const currentElement = input[i]
-  if (currentElement === "e" || currentElement === "E") {
-  
-      result++
-  }
-}
-return result
 }
 
-
-
-export function aufgabe04(args){
-
+export function aufgabe03(args) {
   const input = args
-  let result = 0 
+  let result = 0
   for (let i = 0; i < input.length; i++) {
-  const currentElement = input[i]
+    const currentElement = input[i]
     if (currentElement === "e" || currentElement === "E") {
-  
       result++
+    }
+  }
+  return result
+}
+
+export function aufgabe04(args) {
+  const input = args
+  let result = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === " " && input[i - 1] != "-") {
+      result++
+    }
+  }
+  return result + 1
+}
+
+export function aufgabe05(args) {
+  const input = args
+  if (input.toLowerCase() === input) {
+    return false
+  } else {
+    return true
   }
 }
-return result
+
+export function aufgabe06(args) {
+  const input = args
+  if (input === "") {
+    return false
+  }
+  for (let i = 0; i < input.lenght; i++) {
+    const currentElement = input[i].toLowerCase()
+    if (currentElement.charCodeAt() < 97 || currentElement.charCodeAt() > 122) {
+      if (currentElement.charCodeAt() != 32) {
+        return true
+      }
+    }
+  }
+
+  return false
 }
